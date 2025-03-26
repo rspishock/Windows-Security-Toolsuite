6b51b0411163e0bb3c8f5418ee8ff412e4fade6e
Ryan <22054759+rspishock@users.noreply.github.com>
2025-03-26 19:05:03 -0400

    Update SysInfo.bat
    Updated header

--- SysInfo.bat
+++ SysInfo.bat
@@ -2,8 +2,6 @@ rem *******************************************************
 rem Script: SysInfo.bat
 rem Creation Date: 2/28/2008
 rem Last Modified: 3/15/2008
-rem Author: William R. Stanek
-rem E-mail: williamstanek@aol.com
 rem *******************************************************
 rem Description: Displays system configuration information
 rem              including system name, IP configuration
